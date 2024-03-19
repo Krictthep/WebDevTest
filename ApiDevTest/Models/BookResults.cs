7dd76dd02bc8d5546e7bd1b522cb03bdfa4cefa0
krictthep <krictthep@gmail.com>
2024-03-19 18:36:12 +0700

    New api

---
+++ ApiDevTest/Models/BookResults.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ApiDevTest.Models
+{
+    [Keyless]
+    public class BookResults
+    {
+
+        public string? Title { get; set; }
+
+        public string? AuthorName { get; set; }
+
+        public int AuthorCount { get; set; }
+
+    }
+
+}
---
+++ ApiDevTest/Models/Data/BookContext.cs
@@ -0,0 +1,18 @@
+﻿using ApiDevTest.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiDevTest.Models.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<BookResults> Books { get; set; }
+
+
+        public DbSet<AuthorResults> Authors { get; set; }
+    }
+}
